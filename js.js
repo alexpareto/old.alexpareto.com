b3ffdd2852070b925edf8b28ab286b5e1214c5c0
Alex Pareto <alex.pareto@gmail.com>
2016-08-09 01:51:55 -0700

    website refresh

--- js.js
+++ js.js
@@ -1,11 +1,9 @@
-$(function() {
-  console.log( 'ready!' );
 
-
-  $("#lights").click(function() {
-    $("body").css("background-color", "black");
-    $(".section").css("background-color", "black");
-    $("#toChange").css("color", "white");
-    $("#toChange").text("You might just see something important");
-  });
-});
+var aboutholder = document.getElementsByClassName("aboutholder");
+var array = ["#9ad3de", "#89bdd3", "#dbe9d8", "#b0aac2", "#dbc3d0", "#4D7EA8"];
+var number = Math.floor(Math.random() * array.length);
+aboutholder[0].style.background = array[number];
+document.onclick = function(e){
+    var number = Math.floor(Math.random() * array.length);
+    aboutholder[0].style.background = array[number];
+}
