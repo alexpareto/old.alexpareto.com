02f192454084f2dd4dd2a186f4fdfdefa7c24344
Alex Pareto <alex.pareto@gmail.com>
2016-08-09 01:59:18 -0700

    added preloader

--- js.js
+++ js.js
@@ -7,3 +7,22 @@ document.onclick = function(e){
     var number = Math.floor(Math.random() * array.length);
     aboutholder[0].style.background = array[number];
 }
+<!--//--><![CDATA[//><!--
+var images = new Array()
+function preload() {
+    for (i = 0; i < preload.arguments.length; i++) {
+        images[i] = new Image()
+        images[i].src = preload.arguments[i]
+    }
+}
+preload(
+    "images/Facebook-color.svg",
+    "images/Github-color.svg",
+    "images/GooglePlus-color.svg",
+    "images/Instagram-color.svg",
+    "images/LinkedIn-color.svg",
+    "images/Spotify-color.svg",
+    "images/Twitter-color.svg",
+    "images/usc-color.png"
+)
+//--><!]]>
