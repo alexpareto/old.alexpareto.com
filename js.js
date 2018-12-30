f5e5beb9da78cc94d93ee2673a01631bfe208d9f
Alex Pareto <alex.pareto@gmail.com>
2018-12-30 15:15:04 -0800

    update some things

--- js.js
+++ js.js
@@ -1,12 +1,5 @@
 
 var aboutholder = document.getElementsByClassName("aboutholder");
-var array = ["#9ad3de", "#89bdd3", "#dbe9d8", "#b0aac2", "#dbc3d0", "#4D7EA8"];
-var number = Math.floor(Math.random() * array.length);
-aboutholder[0].style.background = array[number];
-document.onclick = function(e){
-    var number = Math.floor(Math.random() * array.length);
-    aboutholder[0].style.background = array[number];
-}
 <!--//--><![CDATA[//><!--
 var images = new Array()
 function preload() {
